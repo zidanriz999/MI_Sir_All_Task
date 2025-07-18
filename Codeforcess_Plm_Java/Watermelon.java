7e55e4c113a8ed08e4939611667d35cca57d8649
Md.Rashidul Islam Zidan <Rashidulislamzidan141413579@gmail.com>
2025-07-19 00:46:31 +0600

    watermelon_code

---
+++ Codeforcess_Plm_Java/Watermelon.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Watermelon {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int w = scanner.nextInt();
+        if (w % 2 == 0 && w > 2) 
+        
+        {
+            System.out.println("Yes");
+        } 
+        
+        else 
+        {
+            System.out.println("NO");
+        }
+        scanner.close();
+    }
+}
