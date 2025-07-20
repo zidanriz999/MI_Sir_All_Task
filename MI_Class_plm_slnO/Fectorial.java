b03732d83e3606045b7daceb7e5b980bf4cc021c
Md.Rashidul Islam Zidan <Rashidulislamzidan141413579@gmail.com>
2025-07-20 16:17:18 +0600

    class_plm

---
+++ MI_Class_plm_slnO/Fectorial.java
@@ -0,0 +1,16 @@
+package MI_Class_plm_slnO;
+
+public class Fectorial {
+    public static void main(String[] args) {
+        int i;
+        int number=4,fectorial=1;
+        for(i=1;i<=4;i++)
+        {
+            fectorial=fectorial*i;
+        }
+        System.out.println("4! = "+fectorial);
+
+  
+}
+
+}
