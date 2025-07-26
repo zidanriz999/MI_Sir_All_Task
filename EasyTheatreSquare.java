d1b698fd19e7bb526b611709c7f95ae8d872a30f
Md.Rashidul Islam Zidan <Rashidulislamzidan141413579@gmail.com>
2025-07-26 23:23:28 +0600

    ok

---
+++ EasyTheatreSquare.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class EasyTheatreSquare {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    long n = sc.nextLong();
+    long m = sc.nextLong();
+    long a = sc.nextLong();
+
+    long rows = (n + a - 1) / a;
+    long cols = (m + a - 1) / a;
+
+    System.out.println(rows * cols);
+  }
+}
