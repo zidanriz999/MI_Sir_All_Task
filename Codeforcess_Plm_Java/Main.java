7902ae0f954467a77bca55dcc2371bc76780b078
Md.Rashidul Islam Zidan <Rashidulislamzidan141413579@gmail.com>
2025-07-28 02:15:54 +0600

    ok

---
+++ Codeforcess_Plm_Java/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        while (true) {
+            n++;
+            String x = "" + n;
+            if (x.charAt(0) != x.charAt(1) &&
+                x.charAt(0) != x.charAt(2) &&
+                x.charAt(0) != x.charAt(3) &&
+                x.charAt(1) != x.charAt(2) &&
+                x.charAt(1) != x.charAt(3) &&
+                x.charAt(2) != x.charAt(3)) {
+                System.out.println(n);
+                break;
+            }
+        }
+    }
+}
