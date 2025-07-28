07923f4e490312059c80d61b0333e11db61b2814
Md.Rashidul Islam Zidan <Rashidulislamzidan141413579@gmail.com>
2025-07-28 15:48:51 +0600

    ok

---
+++ MI_Class_plm_slnO/array.java
@@ -0,0 +1,14 @@
+public class array {
+  public static void main(String[] args) {
+
+    int[] array = { 1, 2, 3, 4, 5 };
+    int n = array.length;
+    for (int i = 0; i < n; i++) {
+
+      System.out.println("Elements are =" + array[i]);
+
+    }
+    System.out.println("size of array is = " + n);
+  }
+
+}
