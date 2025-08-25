3eb724f94a056726e0f92ca82a1c2caeb220d133
Md.Rashidul Islam Zidan <Rashidulislamzidan141413579@gmail.com>
2025-08-25 15:05:54 +0600

    ok

---
+++ MI_Class_plm_slnO/Lab.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Lab {
+    public static void main(String[] args) {
+     Scanner sc = new Scanner(System.in);   
+     int n = sc.nextInt();
+      for (int i = 0; i < n; i++) {
+          int x = sc.nextInt();   
+          System.out.println("1 " + x);  
+      }
+  
+    }
+    
+  }
