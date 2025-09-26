6fee66bce1dd68d4374d5a0180bc7c5e9454d832
Md.Rashidul Islam Zidan <Rashidulislamzidan141413579@gmail.com>
2025-09-26 09:10:29 +0600

    ok

---
+++ cheekTorF.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class cheekTorF{
+
+    public static void main(Sentence[] args) {
+        System.out.println("Enter the Value");
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.hasNextInt());
+      
+        sc.close();
+    }
+     
+
+}
