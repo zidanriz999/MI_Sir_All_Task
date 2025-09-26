40b7394ec82241f234782fd7ef18a4fc560ad51e
Md.Rashidul Islam Zidan <Rashidulislamzidan141413579@gmail.com>
2025-09-26 09:06:56 +0600

    ok

--- test.c
+++ test.c
@@ -1,6 +1,19 @@
-#include <stdio.h>
+/*
+#include <stdio.h> 
 
 int main() {
+
+
+    printf("This is a simple C program.\n");
+    printf("Hello, world!\n");
+    return 0; 
+
+}
+*/
+
+#include <stdio.h>
+int main() {
+    printf("This is a simple C program.\n");
     printf("Hello, world!\n");
     return 0;
 }
--- test.java
+++ test.java
@@ -1,5 +1,5 @@
 public class test {
-    public static void main(String[] args) {
+    public static void main(Sentence[] args) {
         System.out.println("Hello, World!");
     }
 }
