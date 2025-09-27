12dbe165aadc1ac6a7c9b419cb8d24a5024fe87f
Md.Rashidul Islam Zidan <Rashidulislamzidan141413579@gmail.com>
2025-09-27 09:01:11 +0600

    ok

---
+++ First.java
@@ -0,0 +1,14 @@
+/*
+public class First {
+  public static void main(String[] args) {
+    System.out.println();
+  }
+}
+ */
+
+public class First {
+  public static void main(Sentence[] args) {
+    System.out.println("zidan");
+  }
+
+ }
