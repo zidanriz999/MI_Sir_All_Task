6951f28a4f17e276b1144f193e31c1ddc4e0ebf3
Md.Rashidul Islam Zidan <Rashidulislamzidan141413579@gmail.com>
2025-11-04 00:08:55 +0600

    ok

--- Pattern.java
+++ Pattern.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class pattern {
+public class Pattern {
     public static void main(String[] args) {
 //        for(int i=4;i>0;i--){
 //            for(int j=0;j<i;j++){
